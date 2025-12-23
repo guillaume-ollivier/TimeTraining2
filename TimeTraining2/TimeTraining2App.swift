17df94e2a19ccbed5df82916a6e12f984f9b13fb
Guillaume OLLIVIER <guillaume2@ollivier.eu>
2025-12-23 18:41:52 +0100

    initialisation

---
+++ TimeTraining2/TimeTraining2App.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTraining2App.swift
+//  TimeTraining2
+//
+//  Created by Guillaume OLLIVIER on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeTraining2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeTraining2Tests/TimeTraining2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTraining2Tests.swift
+//  TimeTraining2Tests
+//
+//  Created by Guillaume OLLIVIER on 23/12/2025.
+//
+
+import Testing
+@testable import TimeTraining2
+
+struct TimeTraining2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
